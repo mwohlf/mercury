454d7c1a3c6c9da16ccefa72ba18ec9145446069
michael <michael@wohlfart.net>
2017-10-03 22:56:23 +0200

    reactive form

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+// Angular Material
+import {MdCardModule, MdInputModule, MdSidenavModule, MdButtonModule, MdChipsModule, MdListModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './components/app/app.component';
-import { LoginComponent } from './components/app/login/login.component';
-import { AuthService } from './services/auth.service';
-import { LabelComponent } from "./components/app/label/label.component";
-import { FooterComponent } from "./components/app/footer/footer.component";
-import { MenuComponent } from "./components/app/menu/menu.component";
-import { ToolbarComponent } from "./components/app/toolbar/toolbar.component";
+
+import {AppComponent} from './components/app/app.component';
+import {LoginComponent} from './components/app/login/login.component';
+import {AuthService} from './services/auth.service';
+import {LabelComponent} from "./components/app/label/label.component";
+import {FooterComponent} from "./components/app/footer/footer.component";
+import {MenuComponent} from "./components/app/menu/menu.component";
+import {ToolbarComponent} from "./components/app/toolbar/toolbar.component";
 import {RegisterComponent} from "./components/app/register/register.component";
 import {SettingsComponent} from "./components/app/settings/settings.component";
 import {AdminComponent} from "./components/app/admin/admin.component";
@@ -30,9 +34,12 @@ import {Routing} from "./routing.module";
         AdminComponent,
     ],
     imports: [
+        MdInputModule,
+        MdCardModule,
         BrowserModule,
         BrowserAnimationsModule,
         FormsModule,
+        ReactiveFormsModule,
         HttpModule,
         Routing,
     ],
--- frontend/src/app/components/app/login/login.component.ts
+++ frontend/src/app/components/app/login/login.component.ts
@@ -1,51 +1,51 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {AuthService} from "../../../services/auth.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
-    templateUrl: 'login.component.html'
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
-
 export class LoginComponent implements OnInit {
 
-    public static RETURN_URL = 'returnUrl';
-
-    model: any = {};
-
-    loading = false;
+    loginForm: FormGroup;
 
     returnUrl: string;
 
     constructor(private route: ActivatedRoute,
                 private router: Router,
-                private authService: AuthService /*,
-                private messageService: MessageService*/) {
-        console.log("<constructor>");
+                private authService: AuthService) {
     }
 
     ngOnInit() {
-        // reset login status
-        this.authService.logout();
-        // get return url from route parameters or default to '/'
-        this.returnUrl = this.route.snapshot.queryParams[LoginComponent.RETURN_URL] || '/';
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+        this.createForm();
     }
 
-    login() {
+    private createForm() {
+        this.loginForm = new FormGroup({
+            username: new FormControl('', Validators.required),
+            password: new FormControl('', Validators.required)
+        });
+    }
 
-        this.loading = true;
-        this.authService.login(this.model.username, this.model.password)
+    public login() {
+
+        this.authService.login(
+            this.loginForm.controls['username'].value,
+            this.loginForm.controls['password'].value)
             .subscribe(
                 data => {
                     console.info(data);
-                   // this.messageService.add({severity:'info', summary:'Info Message', detail:'login validated'})
-                    this.loading = false;
+                    // this.messageService.add({severity:'info', summary:'Info Message', detail:'login validated'})
                     this.router.navigate([this.returnUrl]);
                 },
                 error => {
                     console.info(error);
-                   // this.messageService.add({severity:'error', summary:'Info Message', detail:'login invalid'})
-                    this.loading = false;
+                    // this.messageService.add({severity:'error', summary:'Info Message', detail:'login invalid'})
                 });
-
     }
+
 }
