9e5b7e6905906b755b952c68630c9820328c892c
mwohlfart <michael.wohlfart@bridging-it.de>
2017-10-27 17:15:44 +0200

    build fixes

--- backend/src/test/java/net/wohlfart/mercury/util/DummyDataGenerator.java
+++ backend/src/test/java/net/wohlfart/mercury/util/DummyDataGenerator.java
@@ -4,6 +4,7 @@ import net.wohlfart.mercury.model.Role;
 import net.wohlfart.mercury.model.User;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class DummyDataGenerator {
@@ -11,10 +12,13 @@ public class DummyDataGenerator {
     public static List<User> getUsers(int amount, int idStarsFrom) {
         List<User> users = new ArrayList<>();
         for (int i = idStarsFrom; i < amount + idStarsFrom; i++) {
-            users.add(new User("random-name" + i,
-                    String.format("random%d@email.com", i),
-                    "password" + i,
-                    new Role(0L, "USER")));
+            users.add(User.builder()
+                    .name(String.format("random-name%d", i))
+                    .email(String.format("random%d@email.com", i))
+                    .password(String.format("password%d", i))
+                    .roles(Collections.singleton(Role.builder().id(0L).name("USER").build()))
+                    .build()
+            );
             users.get(i - idStarsFrom).setId((long) i);
         }
 
